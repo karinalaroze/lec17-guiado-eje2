87ffe382a08e5498d19bab170828aac768fedef3
KarinaLaroze <karinalaroze@gmail.com>
2017-05-09 12:07:47 -0300

    comenzando ejercicio guiado 2

---
+++ main.js
@@ -0,0 +1,30 @@
+
+ var productos =[
+ {
+ 	nombre: 'pijama',
+ 	tipo: 'ropa'
+ },
+  {
+  	nombre: 'nevera',
+  	tipo: 'electrodoméstico'
+  },
+  {
+  	nombre: 'asus',
+  	tipo: 'computador'
+  },
+  {
+  	nombre: 'macbook',
+  	tipo: 'computador'
+  },
+  {
+  	nombre: 'pijama',
+  	tipo: 'ropa'
+  },
+  {
+  	nombre: 'toshiba', tipo: 'computador'
+  }
+ ];
+
+ //forma imperativa 
+
+ 
